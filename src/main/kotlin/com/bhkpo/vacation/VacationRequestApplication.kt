454e65b292e519e1292484b5612d8fc8644b8b8a
jonusHK <positipman@gmail.com>
2023-05-29 12:41:47 +0900

    Add: init

---
+++ src/main/kotlin/com/bhkpo/vacation/VacationRequestApplication.kt
@@ -0,0 +1,11 @@
+package com.bhkpo.vacation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VacationRequestApplication
+
+fun main(args: Array<String>) {
+    runApplication<VacationRequestApplication>(*args)
+}
